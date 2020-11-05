86f5358851233fba5def73c79b8a4a8162a92aba
RealistKilla <tyler.thms@gmail.com>
2020-11-05 23:06:59 +0200

    Re added initial point cloud model, in potree format.

---
+++ gk/cloud.js
@@ -0,0 +1,104 @@
+{
+	"version": "2.0",
+	"name": "gk",
+	"description": "",
+	"points": 3791052,
+	"projection": "",
+	"hierarchy": {
+		"firstChunkSize": 2310, 
+		"stepSize": 4, 
+		"depth": 9
+	},
+	"offset": [-32.363399999999956, -11.329799999999992, -2.785499999999999],
+	"scale": [0.0001, 0.0001, 0.0001],
+	"spacing": 1.1175484375,
+	"boundingBox": {
+		"min": [-32.363399999999956, -11.329799999999992, -2.785499999999999], 
+		"max": [110.68280000000004, 131.71640000000002, 140.26069999999999]
+	},
+	"encoding": "DEFAULT",
+	"attributes": [
+		{
+			"name": "position",
+			"description": "",
+			"size": 12,
+			"numElements": 3,
+			"elementSize": 4,
+			"type": "int32",
+			"min": [-32.363399999999956, -11.329799999999992, -2.785499999999999],
+			"max": [110.68280000000004, 53.927300000000002, 42.554200000000009]
+		},{
+			"name": "intensity",
+			"description": "",
+			"size": 2,
+			"numElements": 1,
+			"elementSize": 2,
+			"type": "uint16",
+			"min": [1028],
+			"max": [65535]
+		},{
+			"name": "return number",
+			"description": "",
+			"size": 1,
+			"numElements": 1,
+			"elementSize": 1,
+			"type": "uint8",
+			"min": [1],
+			"max": [1]
+		},{
+			"name": "number of returns",
+			"description": "",
+			"size": 1,
+			"numElements": 1,
+			"elementSize": 1,
+			"type": "uint8",
+			"min": [1],
+			"max": [1]
+		},{
+			"name": "classification",
+			"description": "",
+			"size": 1,
+			"numElements": 1,
+			"elementSize": 1,
+			"type": "uint8",
+			"min": [0],
+			"max": [0]
+		},{
+			"name": "scan angle rank",
+			"description": "",
+			"size": 1,
+			"numElements": 1,
+			"elementSize": 1,
+			"type": "uint8",
+			"min": [0],
+			"max": [0]
+		},{
+			"name": "user data",
+			"description": "",
+			"size": 1,
+			"numElements": 1,
+			"elementSize": 1,
+			"type": "uint8",
+			"min": [0],
+			"max": [0]
+		},{
+			"name": "point source id",
+			"description": "",
+			"size": 2,
+			"numElements": 1,
+			"elementSize": 2,
+			"type": "uint16",
+			"min": [1],
+			"max": [1]
+		},{
+			"name": "rgb",
+			"description": "",
+			"size": 6,
+			"numElements": 3,
+			"elementSize": 2,
+			"type": "uint16",
+			"min": [1028, 514, 0],
+			"max": [65535, 65535, 65535]
+		}
+	]
+}
